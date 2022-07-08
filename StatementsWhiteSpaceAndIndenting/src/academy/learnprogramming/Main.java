d5383daf965049151e42a645fd3baacf2c9621ef
CrimsonCoder42 <nostro37@gmail.com>
2022-07-08 11:25:01 -0400

    If, key, CodeBlock

--- StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/academy/learnprogramming/Main.java
@@ -3,6 +3,18 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test " + myVariable);
+
+        System.out.println("This is " +
+                "another " +
+                "still more");
+
+
+
+
     }
 }
