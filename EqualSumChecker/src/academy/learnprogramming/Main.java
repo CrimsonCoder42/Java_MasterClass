2aece13bd78fdfc5970ff77a06a7a88a3a6bf31f
CrimsonCoder42 <nostro37@gmail.com>
2022-07-12 08:15:37 -0400

    Teen Number Check

---
+++ EqualSumChecker/src/academy/learnprogramming/Main.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(hasEqualSum(1,1,2));
+
+    }
+
+    public static boolean hasEqualSum(int a, int b, int c){
+
+        return (a + b) == c;
+    }
+
+}
