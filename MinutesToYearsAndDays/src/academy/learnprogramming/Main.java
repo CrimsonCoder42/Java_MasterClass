1ff39ae181087daf1ddf073e14c13c6eef8d11fe
CrimsonCoder42 <nostro37@gmail.com>
2022-07-14 14:42:43 -0400

    solved

---
+++ MinutesToYearsAndDays/src/academy/learnprogramming/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printYearsAndDays(-561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+
+            long year = minutes / 525600;
+            long days = (minutes % 525600) /  1440;
+
+            System.out.println(minutes + " min = " + year + " y " + "and " + days + " d" );
+
+        }
+
+
+    }
+
+
+}
