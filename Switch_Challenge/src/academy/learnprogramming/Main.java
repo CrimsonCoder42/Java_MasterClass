c796dc8d5776381529c7713671968a77962551a5
CrimsonCoder42 <nostro37@gmail.com>
2022-07-15 19:55:27 -0400

    solved

---
+++ Switch_Challenge/src/academy/learnprogramming/Main.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char character = 'B';
+
+        switch(character){
+
+            case 'B':
+                System.out.println("Is the char B");
+                break;
+
+            case 'C':
+                System.out.println("Is the case C");
+                break;
+
+            case 'D':
+                System.out.println("Is the case C");
+                break;
+
+            default:
+                System.out.println("Was not 'A' 'B' or 'C'");
+
+
+        }
+
+    }
+}
