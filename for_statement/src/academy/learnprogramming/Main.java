62aceb760103631e6f746ae8bb757bfeb524cc5e
CrimsonCoder42 <nostro37@gmail.com>
2022-07-17 10:45:11 -0400

    complete

--- for_statement/src/academy/learnprogramming/Main.java
+++ for_statement/src/academy/learnprogramming/Main.java
@@ -4,14 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000, 2));
+//        System.out.println("10,000 at 2% interest = " + calculateInterest(10000, 2));
 
-        for(int i=2; i < 9; i++){
 
-            double returnedAnswer = calculateInterest(10000, i);
-            System.out.printf("10,000 at %d interest = %f\n", i, returnedAnswer);
+        int primeFound = 0;
+
+        for(int i=1; i < 100; i++){
+            boolean prime = isPrime(i);
+
+            if(prime) {
+                primeFound ++;
+                System.out.println(i);
+            }
+            if(primeFound == 15){
+                break;
+            }
         }
     }
+
+    public static boolean isPrime(int n) {
+
+        if(n==1) {
+            return false;
+        }
+
+        for(int i=2; i<=n/2; i++){
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
 
     public static double calculateInterest(double amount, double interestRate) {
 
