de1d2b7e690788ca7a02c89528352f119945dae0
CrimsonCoder42 <nostro37@gmail.com>
2022-07-17 17:56:37 -0400

    do and do while challenge

--- WhileAndDoWhile/src/academy/learnprogramming/Main.java
+++ WhileAndDoWhile/src/academy/learnprogramming/Main.java
@@ -4,22 +4,56 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int count = 1;
-        while(count != 6) {
-            System.out.println("count value is " + count);
-            count++;
+//        int count = 1;
+//        while(count != 6) {
+//            System.out.println("count value is " + count);
+//            count++;
+//
+//        }
+//
+//        count = 0;
+//        while(true) {
+//            if(count == 5) {
+//                break;
+//            }
+//
+//            System.out.println("Count value = " + count);
+//            count++;
+//        }
 
-        }
+//        int count = 1;
+//        do{
+//            System.out.println("Count value was " + count);
+//            count++;
+//        } while(count !=6);
 
-        count = 0;
-        while(true) {
-            if(count == 5) {
-                break;
+        int number =4;
+        int finsihNumber = 20;
+
+        while(number <= finsihNumber) {
+
+            number++;
+
+            if (!isEvenNumber(number)) {
+                continue;
             }
 
-            System.out.println("Count value = " + count);
-            count++;
+            System.out.println("Even number " + number);
         }
 
     }
+
+    public static boolean isEvenNumber(int num) {
+
+        if (num < 1) {
+            return false;
+        }
+
+        if(num % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
